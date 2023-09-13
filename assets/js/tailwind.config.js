076eadd806f258ba418c3e6dfc3006c722b2e7c6
azizrakhimjonov <azizjonraximjonov7@gmail.com>
2023-09-13 18:51:38 +0500

    index ochildi

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+tailwind.config = {
+    theme: {
+      container: {
+        padding: {
+          DEFAULT: '10px',
+          sm:'1rem'
+        },
+      },
+      extend: {
+        screens: {
+          'xs':'475px',
+          // => @media (min-width: 450px) { ... }
+        },
+      }
+    }
+  }
